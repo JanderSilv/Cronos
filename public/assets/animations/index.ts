08e76e35295a237d8457fa8ad5b22c772346a125
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-04-27 12:47:13 -0300

    feat: add animations

---
+++ public/assets/animations/index.ts
@@ -0,0 +1,4 @@
+export { default as SunAnimation } from './sun.json'
+export { default as LunchAnimation } from './lunch.json'
+export { default as MoonAnimation } from './moon.json'
+export { default as WorkingAnimation } from './working.json'
