b25a29c464772b6595ef8640140a117f4ca986d4
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-04-27 22:58:14 -0300

    feat: add Animation component

---
+++ src/components/Animation/index.tsx
@@ -0,0 +1,41 @@
+import Lottie from 'react-lottie'
+import {
+  LunchAnimation,
+  NightAnimation,
+  SunAnimation,
+  WorkingAnimation
+} from '../../../public/assets/animations'
+
+interface Props {
+  period: string
+  status: number
+}
+
+const Animation = ({ period, status }: Props): JSX.Element => {
+  const chooseAnimation = () => {
+    if (status === 1) return WorkingAnimation
+    switch (period.toLowerCase()) {
+      case 'bom dia':
+      case 'boa tarde':
+        return SunAnimation
+      case 'boa madrugada':
+      case 'boa noite':
+        return NightAnimation
+      case 'bom almoço':
+        return LunchAnimation
+    }
+  }
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: chooseAnimation(),
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
+
+  return <Lottie options={defaultOptions} width={100} />
+}
+
+export default Animation
