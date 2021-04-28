1b829c0e6dee2e2371d22d3a3405075aed295162
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-04-27 22:58:01 -0300

    feat: add night animation

--- public/assets/animations/index.ts
+++ public/assets/animations/index.ts
@@ -1,4 +1,5 @@
 export { default as SunAnimation } from './sun.json'
 export { default as LunchAnimation } from './lunch.json'
 export { default as MoonAnimation } from './moon.json'
+export { default as NightAnimation } from './night.json'
 export { default as WorkingAnimation } from './working.json'
