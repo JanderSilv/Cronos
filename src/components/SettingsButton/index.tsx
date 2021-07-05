bfe7f61f4360e800840e5a804f10a304cd99b63a
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-07-05 18:52:40 -0300

    feat: add SettingsButton

--- src/components/SettingsButton/index.tsx
+++ src/components/SettingsButton/index.tsx
@@ -1,9 +1,84 @@
-import Fab from '@material-ui/core/Fab'
+import { useCallback, useState, memo } from 'react'
+import SpeedDial from '@material-ui/lab/SpeedDial'
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction'
+import { makeStyles } from '@material-ui/core/styles'
 
-// import { Container } from './styles';
+import { useTheme } from '../../hooks/useTheme'
+import { SettingsIcon, MoonIcon, SunIcon } from '../../../public/assets/icons'
 
-const SettingsButton = () => {
-  return <Fab>Olá</Fab>
+const SettingsButton = (): JSX.Element => {
+  const { toggleTheme, currentTheme } = useTheme()
+  const classes = useStyles()
+
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = useCallback(() => setOpen(value => !value), [])
+
+  const makeActions = useCallback(
+    () => [
+      {
+        icon:
+          currentTheme === 'dark' ? (
+            <SunIcon key="sun-icon" />
+          ) : (
+            <MoonIcon key="moon-icon" />
+          ),
+        name: currentTheme === 'dark' ? 'Claro' : 'Escuro',
+        action: toggleTheme
+      }
+    ],
+    [currentTheme, toggleTheme]
+  )
+
+  return (
+    <SpeedDial
+      ariaLabel="configurações"
+      classes={{ fab: classes.fabButton }}
+      icon={<SettingsIcon color="action" />}
+      onClose={handleOpen}
+      onOpen={handleOpen}
+      open={open}
+      direction="up"
+    >
+      {makeActions().map(action => (
+        <SpeedDialAction
+          key={action.name}
+          color="action"
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={action.action}
+          className={classes.actionButton}
+        />
+      ))}
+    </SpeedDial>
+  )
 }
 
-export default SettingsButton
+export default memo(SettingsButton)
+
+const useStyles = makeStyles(theme => ({
+  fabButton: {
+    width: 50,
+    height: 50,
+
+    background: theme.palette.type === 'light' ? 'white' : '#00000061',
+    boxShadow: theme.shadows[1],
+    transition: 'background .2s',
+
+    '&:hover': {
+      background: theme.palette.type === 'light' ? '#d8d8d8' : '#5e5e5e'
+    }
+  },
+  actionButton: {
+    width: 45,
+    height: 45,
+    background: theme.palette.type === 'light' ? 'white' : '#00000061',
+    boxShadow: theme.shadows[1],
+
+    '& svg': {
+      width: '100%',
+      maxWidth: 24,
+      height: 'auto'
+    }
+  }
+}))
